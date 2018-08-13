ae9d571921831fb51329c79df4317e502a8362a9
jihaiying <892425470@qq.com>
2018-08-13 17:55:54 +0800

    remove test husky

--- src/App.js
+++ src/App.js
@@ -13,13 +13,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p className="App-intro">
-          test husky!test husky!test husky!test husky!test husky!test husky!test
-          husky!test husky!test husky!test husky!test husky!test husky!test
-          husky!test husky!test husky!test husky!test husky!test husky!test
-          husky!test husky!test husky!test husky!test husky!test husky!test
-          husky!test husky!test husky!test husky!
-        </p>
       </div>
     );
   }
