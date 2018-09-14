5182b3f71c312642fdd70a77db5114151e9284ff
haiyingji <haiyingji@creditease.cn>
2018-09-14 17:36:03 +0800

    GitHub Pages 配置，BrowserRouter 改为 HashRouter

--- src/components/ForI18n/index.js
+++ src/components/ForI18n/index.js
@@ -5,7 +5,7 @@ import zh_CN from "../../i18n/zh_CN";
 import en_US from "../../i18n/en_US";
 import { LANGUAGES } from "../../config/enum";
 import { connect } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 class ForI18n extends Component {
   render() {
@@ -13,9 +13,9 @@ class ForI18n extends Component {
     const message = lang === LANGUAGES.en ? en_US : zh_CN;
     return (
       <IntlProvider locale="en" messages={message}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </IntlProvider>
     );
   }
